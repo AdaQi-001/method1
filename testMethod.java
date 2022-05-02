368ad544c9bcd47e4ee477706a5d0ff90571fa58
CharlotQi <97003885+CharlotQi@users.noreply.github.com>
2022-05-02 00:14:09 -0700

    editing3

--- testMethod.java
+++ testMethod.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class testMethod {
     @Test
     public void testAddOne(){
-        assertEquals(0,Method.addOne(1)); //Expect to be failed
+        assertEquals(2,Method.addOne(1)); //Expect to be failed
     }
 }
